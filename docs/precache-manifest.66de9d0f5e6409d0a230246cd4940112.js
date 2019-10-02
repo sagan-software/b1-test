26530e805632fcf9a09ebcd2e4cbf47fe543441e
Liam Curry <liam@sagan.software>
2019-10-02 15:59:51 -0400

    added docs

---
+++ docs/precache-manifest.66de9d0f5e6409d0a230246cd4940112.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0d13adceec626bae8ecf9fd4f39e0f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "070fa201b2ebd4bbe67c",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "070fa201b2ebd4bbe67c",
+    "url": "/static/js/2.c23399f3.chunk.js"
+  },
+  {
+    "revision": "53b207dc4ec9181fd098",
+    "url": "/static/js/main.80dfea4d.chunk.js"
+  },
+  {
+    "revision": "f3c12497a93bcca79254",
+    "url": "/static/js/runtime-main.10faea99.js"
+  }
+]);
